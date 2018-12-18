21f8c3380f5bf2363319bcb053b092295c30bf93
jduv <jduv@7factor.io>
2018-12-17 22:46:27 -0500

    Adding KMS integration for testing.

---
+++ kms.tf
@@ -0,0 +1,60 @@
+resource "aws_kms_key" "vault_autounseal" {
+  description = "Vault auto-unseal key."
+
+  policy = <<EOF
+{
+    "Id": "key-consolepolicy-3",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::024124389053:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${aws_iam_role.vault_role.arn}"
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow attachment of persistent resources",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${aws_iam_role.vault_role.arn}"
+            },
+            "Action": [
+                "kms:CreateGrant",
+                "kms:ListGrants",
+                "kms:RevokeGrant"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Bool": {
+                    "kms:GrantIsForAWSResource": "true"
+                }
+            }
+        }
+    ]
+}
+
+EOF
+}
+
+resource "aws_kms_alias" "vault_autounseal_alias" {
+  name          = "alias/vault-autounseal"
+  target_key_id = "${aws_kms_key.vault_autounseal.key_id}"
+}
