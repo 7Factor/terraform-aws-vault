b27c391adbf187719b5945fab8aa731b353b66f2
jduv <jduv@7factor.io>
2018-12-17 22:51:12 -0500

    Updating dynamo permissions to be of least privilige.

--- ec2.tf
+++ ec2.tf
@@ -136,7 +136,25 @@ resource "aws_iam_policy" "vault_policy" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": "dynamodb:*",
+      "Action": [
+        "dynamodb:DescribeLimits",
+        "dynamodb:DescribeTimeToLive",
+        "dynamodb:ListTagsOfResource",
+        "dynamodb:DescribeReservedCapacityOfferings",
+        "dynamodb:DescribeReservedCapacity",
+        "dynamodb:ListTables",
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:CreateTable",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:GetRecords",
+        "dynamodb:PutItem",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem",
+        "dynamodb:Scan",
+        "dynamodb:DescribeTable"
+      ],
       "Resource": "*"
     },
     {
