eb71d2d9a382e5f72487d8a0060c1274217230b9
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 08:21:39 -0400

    Converting to HCL2 expressions.

--- kms.tf
+++ kms.tf
@@ -56,5 +56,5 @@ EOF
 
 resource "aws_kms_alias" "vault_autounseal_alias" {
   name = "alias/vault-autounseal"
-  target_key_id = "${aws_kms_key.vault_autounseal.key_id}"
+  target_key_id = aws_kms_key.vault_autounseal.key_id
 }
