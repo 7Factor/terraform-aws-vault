fb2a9a1719fab3c49caadbbd36666957f1724055
chris-malloy <chrismalloymusic@gmail.com>
2020-06-09 00:38:44 -0400

    No need for ssl on internal lb. [ch15615]

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "lb_dns" {
 }
 
 output "pub_lb_dns" {
-  value = aws_lb.vault_external_lb[*].dns_name
+  value       = aws_lb.vault_external_lb[*].dns_name
   description = "The DNS value of your external ELB hosting the vault cluster. Point your FQDN to it if you are not using a VPN."
 }
